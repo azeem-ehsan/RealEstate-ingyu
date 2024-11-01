0fd7eaf6b447acc7fba047a69efcd34c9b034d34
Zaid <xaid.bin.khalid@gmail.com>
2024-11-01 17:40:16 +0500

    Single floor building added

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -2,21 +2,26 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
-    
+    public bool openInside = true;  // True = open inside, False = open outside
+
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Player")
+        if (other.tag == "Player")
         {
             Debug.Log("Player entered the trigger");
-            LeanTween.rotateY(gameObject, -17, 1f).setEase(LeanTweenType.easeOutQuad).setOnComplete(() => {
+
+            // Decide rotation angle based on the direction of opening
+            float targetRotation = openInside ? -17f : 17f;
+
+            LeanTween.rotateY(gameObject, targetRotation, 1f).setEase(LeanTweenType.easeOutQuad).setOnComplete(() => {
                 Invoke("CloseDoor", 2f);
             });
         }
     }
+    
 
-    void CloseDoor()
-        {
-                LeanTween.rotateY(gameObject, 85, 1f).setEase(LeanTweenType.easeOutQuad);
-
-        }
+    // void CloseDoor()
+    // {
+    //     LeanTween.rotateY(gameObject, 85, 1f).setEase(LeanTweenType.easeOutQuad);
+    // }
 }
